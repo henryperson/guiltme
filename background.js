617a9c20b03a76eaacff394c29d1c261c0fd4a13
Nishad Singh <nishadsingh@berkeley.edu>
2015-05-19 02:20:40 -0700

    Hash updating correctly.

--- background.js
+++ background.js
@@ -1,43 +1,40 @@
-// var instanceVars = {
-// 	last_time: new Date().getTime() / 1000,
-// 	last_url: "example.com",
-// 	url_to_time: {
-// 		"current_url": 0
-// 	},
+var instanceVars = {
+	last_time: new Date().getTime() / 1000,
+	last_url: "example.com",
+	url_to_time: {
+		"current_url": 0
+	},
 
-// 	update_table: function(){
-// 		var now = new Date().getTime() / 1000;
-// 		var difference = now - instanceVars.last_time;
-// 		if(instanceVars.url_to_time.url==undefined){
-// 			instanceVars.url_to_time.url = difference;
-// 		} else {
-// 			instanceVars.url_to_time.url = instanceVars.url_to_time.url + difference;
-// 		}
-// 	},
+	update: function(current_url){
+		var now = new Date().getTime() / 1000;
+		if(instanceVars.url_to_time.url==undefined){
+			instanceVars.url_to_time[instanceVars.last_url] = now - instanceVars.last_time;
+		} else {
+			instanceVars.url_to_time[instanceVars.last_url]  = instanceVars.url_to_time[instanceVars.last_url] + difference;
+		}
+		instanceVars.last_url = current_url;
+		instanceVars.last_time = now;
+	}
 
-// 	update_to_current_url: function(String current_url){
-// 		instanceVars.last_url = current_url;
-// 		instanceVars.last_time = new Date().getTime() / 1000;
-// 	},
-
-// }
+}
 
 chrome.tabs.onActivated.addListener(function(activeInfo){
-	// instanceVars.update_table();
-	// instanceVars.update_to_current_url(tab.url);
-	// console.log(instanceVars.url_to_time);
 	var tabId = activeInfo.tabId;
 	chrome.tabs.get(tabId, function(tab){
 		console.log("tab activate");
-		console.log(tab.url);
+		// console.log(tab.url);
+		instanceVars.update(tab.url);
+		console.log(instanceVars.url_to_time);
 	});
 });
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
 	chrome.tabs.get(tabId, function(tab){
-		if(tab.active && changeInfo=="complete"){
-			console.log("tab activate");
-			console.log(tab.url);	
+		if(tab.active){
+			console.log("tab update");
+			// console.log(tab.url);
+			instanceVars.update(tab.url);
+			console.log(instanceVars.url_to_time);
 		}	
 	});
 });
@@ -48,7 +45,9 @@ chrome.windows.onFocusChanged.addListener(function(windowId){
 			var tab =window.tabs[i];
 			if(tab.active){
 				console.log("window focus");
-				console.log(tab.url);
+				// console.log(tab.url);
+				instanceVars.update(tab.url);
+				console.log(instanceVars.url_to_time);
 			}
 		}
 	});
