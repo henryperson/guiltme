b598ee48eb3ab9dde52cbf73c025bca0efe62eb7
Nishad Singh <nishadsingh@berkeley.edu>
2015-05-20 15:46:12 -0700

    Auth implemented a little better. Need to add a /config/application.yml with auth key.

--- guiltme/app/controllers/learning_controller.rb
+++ guiltme/app/controllers/learning_controller.rb
@@ -1,6 +1,6 @@
 class LearningController < ApplicationController
 	def learn
-		if params[:auth] == 123
+		if params[:auth] == YAML.load_file('config/application.yml')["learning_key"]
 			render json: Learner.learn.to_json
 		else
 			render json: {"error" => "invalid credentials"}.to_json
