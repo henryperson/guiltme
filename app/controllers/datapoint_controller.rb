520a398763a4f864473749c8d956b4b423db99cb
Nishad Singh <nishadsingh@berkeley.edu>
2015-05-25 04:23:46 -0700

    Quick change to MIRA update.

--- app/controllers/datapoint_controller.rb
+++ app/controllers/datapoint_controller.rb
@@ -1,14 +1,14 @@
 class DatapointController < ApplicationController
   respond_to :json
   def create
-    classification = Classification.find_or_create(params[:classification])
-    datapoint = Datapoint.new_or_overwrite(params[:url], classification)
+    new_classification = Classification.find_or_create(params[:classification])
+    datapoint = Datapoint.new_or_overwrite(params[:url], new_classification)
     render json: datapoint
-    correct_classification = Classifier.classify_url(datapoint.url)
-    unless classification.name == correct_classification
-     correct_vector = Classification.find_by_name(correct_classification).vector
-     chosen_vector = classification.vector
-     feature_vector = FeatureVectorCreator.get_vector(datapoint.url)
+    chosen_classification = Classifier.classify_url(datapoint.url)
+    unless new_classification.name == chosen_classification
+      chosen_vector = Classification.find_by_name(chosen_classification).vector
+      correct_vector = new_classification.vector  
+      feature_vector = FeatureVectorCreator.get_vector(datapoint.url)
       PerceptronLearner.mira_update(correct_vector, chosen_vector, feature_vector)
     end
   end
